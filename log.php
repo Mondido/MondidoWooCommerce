829bb09fb44263fc213424cdd86cf25b603de073
Robert Pohl <robert@sugarcubesolutions.com>
2016-09-01 14:17:30 +0200

    upd socket

--- log.php
+++ log.php
@@ -2,14 +2,17 @@
 class Log
 {
     public function send($message, $component = "web", $program = "next_big_thing") {
-     $PAPERTRAIL_HOSTNAME = 'logs4.papertrailapp.com';
-     $PAPERTRAIL_PORT = '20177';
-
-        $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-        foreach(explode("\n", $message) as $line) {
-            $syslog_message = "<22>" . date('M d H:i:s ') . $program . ' ' . $component . ': ' . $line;
-            socket_sendto($sock, $syslog_message, strlen($syslog_message), 0, $PAPERTRAIL_HOSTNAME, $PAPERTRAIL_PORT);
+        $PAPERTRAIL_HOSTNAME = 'logs4.papertrailapp.com';
+        $PAPERTRAIL_PORT = '20177';
+        try {
+            $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+            foreach(explode("\n", $message) as $line) {
+                $syslog_message = "<22>" . date('M d H:i:s ') . $program . ' ' . $component . ': ' . $line;
+                socket_sendto($sock, $syslog_message, strlen($syslog_message), 0, $PAPERTRAIL_HOSTNAME, $PAPERTRAIL_PORT);
+            }
+            socket_close($sock);
+        }  catch (Exception $e) {
+            //socket error
         }
-        socket_close($sock);
     }
 }
