d95c5d7ddbd3a6d1dd80ee811f39f094d8bab225
Robert Pohl <robert@sugarcubesolutions.com>
2016-10-06 22:06:10 +0200

    debug

--- log.php
+++ log.php
@@ -5,12 +5,12 @@ class Log
         $PAPERTRAIL_HOSTNAME = 'logs4.papertrailapp.com';
         $PAPERTRAIL_PORT = '20177';
         try {
-            $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+//            $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
             foreach(explode("\n", $message) as $line) {
                 $syslog_message = "<22>" . date('M d H:i:s ') . $program . ' ' . $component . ': ' . $line;
               //  socket_sendto($sock, $syslog_message, strlen($syslog_message), 0, $PAPERTRAIL_HOSTNAME, $PAPERTRAIL_PORT);
             }
-            socket_close($sock);
+//            socket_close($sock);
         }  catch (Exception $e) {
             //socket error
         }
