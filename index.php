4379dc59f23415476dbb17cd21eebb376f627225
Robert Pohl <robert@sugarcubesolutions.com>
2016-12-30 13:29:23 +0100

    ref

--- index.php
+++ index.php
@@ -26,37 +26,6 @@ add_filter( 'woocommerce_cart_needs_payment', 'cart_needs_payment_filter', 10, 2
 add_filter( 'woocommerce_order_needs_payment', 'order_needs_payment_filter', 10, 3 ); 
 add_filter('woocommerce_order_button_text','custom_order_button_text',1);
 
-add_filter( 'woocommerce_locate_template', 'myplugin_woocommerce_locate_template', 10, 3 );
-function myplugin_plugin_path() {
-  // gets the absolute path to this plugin directory
-  return untrailingslashit( plugin_dir_path( __FILE__ ) );
-}
- 
-function myplugin_woocommerce_locate_template( $template, $template_name, $template_path ) {
-  global $woocommerce;
-  $_template = $template;
-  if ( ! $template_path ) $template_path = $woocommerce->template_url;
-  $plugin_path  = myplugin_plugin_path() . '/woocommerce/';
- 
-  // Look within passed path within the theme - this is priority
-  $template = locate_template(
-    array(
-      $template_path . $template_name,
-      $template_name
-    )
-  );
-  // Modification: Get the template from this plugin, if it exists
-  if ( ! $template && file_exists( $plugin_path . $template_name ) )
-    $template = $plugin_path . $template_name;
-  // Use default template
-  if ( ! $template )
-    $template = $_template;
- 
-  // Return what we found
-  return $template;
-}
-
-
 function custom_order_button_text($order_button_text) {
 	
     $mondido = new WC_Gateway_Mondido();
