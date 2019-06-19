3a76c7076283a99c8142bac138ec59182162451f
Viktor <viktor@codepeack.se>
2019-06-19 16:56:59 +0300

    Fix 37 issues

--- includes/class-wc-gateway-mondido-invoice.php
+++ includes/class-wc-gateway-mondido-invoice.php
@@ -1,16 +1,25 @@
 <?php
+/**
+ * Mondido gateway mondido invoice
+ *
+ * @author Mondido
+ * @package mondido
+ **/
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 } // Exit if accessed directly
 
+/**
+ * Class WC_Gateway_Mondido_Invoice
+ */
 class WC_Gateway_Mondido_Invoice extends WC_Gateway_Mondido_HW {
 	/**
 	 * Init
 	 */
 	public function __construct() {
 		$this->id                 = 'mondido_invoice';
-		$this->has_fields         = TRUE;
+		$this->has_fields         = true;
 		$this->method_title       = __( 'Mondido Invoice', 'woocommerce-gateway-mondido' );
 		$this->method_description = '';
 
@@ -26,7 +35,7 @@ class WC_Gateway_Mondido_Invoice extends WC_Gateway_Mondido_HW {
 		// Load the settings.
 		$this->init_settings();
 
-		// Define variables
+		// Define variables!
 		$this->enabled     = isset( $this->settings['enabled'] ) ? $this->settings['enabled'] : 'no';
 		$this->title       = isset( $this->settings['title'] ) ? $this->settings['title'] : __( 'Mondido Invoice', 'woocommerce-gateway-mondido' );
 		$this->description = isset( $this->settings['description'] ) ? $this->settings['description'] : '';
@@ -40,37 +49,23 @@ class WC_Gateway_Mondido_Invoice extends WC_Gateway_Mondido_HW {
 		$this->logos             = isset( $this->settings['logos'] ) ? $this->settings['logos'] : array();
 		$this->order_button_text = isset( $this->settings['order_button_text'] ) ? $this->settings['order_button_text'] : __( 'Pay with Mondido', 'woocommerce-gateway-mondido' );
 
-		add_filter( 'woocommerce_mondido_form_fields', array(
-			$this,
-			'set_payment_method'
-		), 10, 3 );
+		add_filter( 'woocommerce_mondido_form_fields', array( $this, 'set_payment_method' ), 10, 3 );
 
-		// Actions
-		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array(
-			$this,
-			'process_admin_options'
-		) );
+		// Actions!
+		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
 
-		add_action( 'woocommerce_thankyou_' . $this->id, array(
-			$this,
-			'thankyou_page'
-		) );
+		add_action( 'woocommerce_thankyou_' . $this->id, array(	$this, 'thankyou_page' ) );
 
-		// Payment listener/API hook
-		add_action( 'woocommerce_api_wc_gateway_' . $this->id, array(
-			$this,
-			'notification_callback'
-		) );
+		// Payment listener/API hook!
+		add_action( 'woocommerce_api_wc_gateway_' . $this->id, array( $this, 'notification_callback' ) );
 
-		// Receipt hook
-		add_action( 'woocommerce_receipt_' . $this->id, array(
-			$this,
-			'receipt_page'
-		) );
+		// Receipt hook!
+		add_action( 'woocommerce_receipt_' . $this->id, array( $this, 'receipt_page' ) );
 	}
 
 	/**
 	 * Initialise Settings Form Fields
+     *
 	 * @return void
 	 */
 	public function init_form_fields() {
@@ -79,13 +74,13 @@ class WC_Gateway_Mondido_Invoice extends WC_Gateway_Mondido_HW {
 				'title'   => __( 'Enable/Disable', 'woocommerce-gateway-mondido' ),
 				'type'    => 'checkbox',
 				'label'   => __( 'Enable Mondido Invoice', 'woocommerce-gateway-mondido' ),
-				'default' => 'no'
+				'default' => 'no',
 			),
 			'title'             => array(
 				'title'       => __( 'Title', 'woocommerce-gateway-mondido' ),
 				'type'        => 'text',
 				'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce-gateway-mondido' ),
-				'default'     => __( 'Mondido Invoice', 'woocommerce-gateway-mondido' )
+				'default'     => __( 'Mondido Invoice', 'woocommerce-gateway-mondido' ),
 			),
 			'description'       => array(
 				'title'       => __( 'Description', 'woocommerce-gateway-mondido' ),
@@ -97,48 +92,48 @@ class WC_Gateway_Mondido_Invoice extends WC_Gateway_Mondido_HW {
 				'title'       => __( 'Merchant ID', 'woocommerce-gateway-mondido' ),
 				'type'        => 'text',
 				'description' => __( 'Merchant ID for Mondido', 'woocommerce-gateway-mondido' ),
-				'default'     => ''
+				'default'     => '',
 			),
 			'secret'            => array(
 				'title'       => __( 'Secret', 'woocommerce-gateway-mondido' ),
 				'type'        => 'password',
 				'description' => __( 'Given secret code from Mondido', 'woocommerce-gateway-mondido' ),
-				'default'     => ''
+				'default'     => '',
 			),
 			'password'          => array(
 				'title'       => __( 'API Password', 'woocommerce-gateway-mondido' ),
 				'type'        => 'text',
 				'description' => __( 'API Password from Mondido', 'woocommerce-gateway-mondido' ) . ' (<a href="https://admin.mondido.com/settings">https://admin.mondido.com/settings</a>)',
-				'default'     => ''
+				'default'     => '',
 			),
 			'testmode'          => array(
 				'title'   => __( 'Test Mode', 'woocommerce-gateway-mondido' ),
 				'type'    => 'checkbox',
 				'label'   => __( 'Set in testmode', 'woocommerce-gateway-mondido' ),
-				'default' => 'no'
+				'default' => 'no',
 			),
 			'authorize'         => array(
 				'title'   => __( 'Authorize', 'woocommerce-gateway-mondido' ),
 				'type'    => 'checkbox',
 				'label'   => __( 'Reserve money, do not auto-capture', 'woocommerce-gateway-mondido' ),
-				'default' => 'no'
+				'default' => 'no',
 			),
 			'tax_status'        => array(
 				'title'       => __( 'Tax status for payment fees', 'woocommerce-gateway-mondido' ),
 				'type'        => 'select',
 				'options'     => array(
 					'none'    => __( 'None', 'woocommerce-gateway-mondido' ),
-					'taxable' => __( 'Taxable', 'woocommerce-gateway-mondido' )
+					'taxable' => __( 'Taxable', 'woocommerce-gateway-mondido' ),
 				),
 				'description' => __( 'If any payment fee should be taxable', 'woocommerce-gateway-mondido' ),
-				'default'     => 'none'
+				'default'     => 'none',
 			),
 			'tax_class'         => array(
 				'title'       => __( 'Tax class for payment fees', 'woocommerce-gateway-mondido' ),
 				'type'        => 'select',
 				'options'     => self::getTaxClasses(),
 				'description' => __( 'If you have a fee for invoice payments, what tax class should be applied to that fee', 'woocommerce-gateway-mondido' ),
-				'default'     => 'standard'
+				'default'     => 'standard',
 			),
 			'order_button_text' => array(
 				'title'   => __( 'Text for "Place Order" button', 'woocommerce-gateway-mondido' ),
@@ -161,8 +156,9 @@ class WC_Gateway_Mondido_Invoice extends WC_Gateway_Mondido_HW {
 	/**
 	 * Set Payment Method on mondido form
 	 * @param array $fields
+     *
 	 * @param WC_Order $order
-	 * @param WC_Payment_Gateway $gateway
+     * @param WC_Payment_Gateway $gateway
 	 *
 	 * @return array
 	 */
@@ -172,12 +168,12 @@ class WC_Gateway_Mondido_Invoice extends WC_Gateway_Mondido_HW {
         }
 
 		if ( $order->get_payment_method() === $this->id) {
-			$fields['payment_method'] = 'invoice';
+			$fields ['payment_method'] = 'invoice';
 		}
 
 		return $fields;
 	}
 }
 
-// Register Gateway
+// Register Gateway!
 WC_Mondido_Payments::register_gateway( 'WC_Gateway_Mondido_Invoice' );
