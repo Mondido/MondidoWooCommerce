7646fea3b7a983c1cf1ac1953374cc01d9ccc372
Timo <timo@mondido.com>
2020-09-15 10:01:34 +0200

    remove old gateways and fix double mondido in title

--- woocommerce-gateway-mondido.php
+++ woocommerce-gateway-mondido.php
@@ -129,12 +129,12 @@ CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}mondido_customers` (
 
 		include_once( dirname( __FILE__ ) . '/includes/class-wc-payment-token-mondido.php' );
 
-		WC_Mondido_Payments::register_preselect_gateway('swish', 'Mondido Swish', 'Pay with Swish');
-		WC_Mondido_Payments::register_preselect_gateway('bank', 'Mondido Direct Bank', 'Pay with Direct Bank');
+		WC_Mondido_Payments::register_preselect_gateway('swish', 'Swish', 'Pay with Swish');
+		WC_Mondido_Payments::register_preselect_gateway('bank', 'Direct Bank', 'Pay with Direct Bank');
 		WC_Mondido_Payments::register_gateway( new WC_Gateway_Mondido_HW() );
-		WC_Mondido_Payments::register_preselect_gateway('credit_card', 'Mondido Card', 'Pay with Card');
-		WC_Mondido_Payments::register_preselect_gateway('paypal', 'Mondido PayPal', 'Pay with PayPal');
-		WC_Mondido_Payments::register_preselect_gateway('invoice', 'Mondido Invoice', 'Pay with Mondido');
+		WC_Mondido_Payments::register_preselect_gateway('credit_card', 'Card', 'Pay with Card');
+		WC_Mondido_Payments::register_preselect_gateway('paypal', 'PayPal', 'Pay with PayPal');
+		WC_Mondido_Payments::register_preselect_gateway('invoice', 'Invoice', 'Pay with Mondido');
 	}
 
 	/**
