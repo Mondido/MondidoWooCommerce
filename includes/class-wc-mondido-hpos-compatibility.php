0b9d8dd933fc0c846da7cc88a862034d9cb1dadd
Max Frederiksen <max@mondido.com>
2024-04-12 09:57:43 +0200

    refactor hpos class structure

--- includes/class-wc-mondido-hpos-compatibility.php
+++ includes/class-wc-mondido-hpos-compatibility.php
@@ -5,15 +5,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 use Automattic\WooCommerce\Utilities\OrderUtil;
 
-interface OrderStorageInterface {
-    public function get_meta_data($order, $meta_key, $meta_value);
-    public function add_meta_data($order, $meta_key, $meta_value);
-    public function update_meta_data($order, $meta_key, $meta_value);
-    public function delete_meta_data($order, $meta_key);
-    public function save($order);
+abstract class OrderStorage {
+    public static function current(): OrderStorage {
+		if (OrderUtil::custom_orders_table_usage_is_enabled()) {
+			return new HPOSOrderStorage();
+		} else {
+			return new CPTOrderStorage();
+		}
+	}
+
+	public abstract function get_meta_data($order, $meta_key, $meta_value);
+	public abstract function add_meta_data($order, $meta_key, $meta_value);
+	public abstract function update_meta_data($order, $meta_key, $meta_value);
+	public abstract function delete_meta_data($order, $meta_key);
+	public abstract function save($order);
 }
 
-class HPOSOrderStorage implements OrderStorageInterface {
+class HPOSOrderStorage implements OrderStorage {
     public function get_meta_data($order, $meta_key, $meta_value) {
         return $order->get_meta($meta_key, $meta_value);
     }
@@ -35,7 +43,7 @@ class HPOSOrderStorage implements OrderStorageInterface {
     }
 }
 
-class CPTOrderStorage implements OrderStorageInterface {
+class CPTOrderStorage implements OrderStorage {
     public function get_meta_data($order, $meta_key, $meta_value) {
         return get_post_meta($order->get_id(), $meta_key, $meta_value);
     }
@@ -54,15 +62,5 @@ class CPTOrderStorage implements OrderStorageInterface {
 
     public function save($order) {
         return; // Do nothing, CPT doesn't use save
-    }
-}
-
-class OrderStorage {
-    public static function current() {
-        if (OrderUtil::custom_orders_table_usage_is_enabled()) {
-            return new HPOSOrderStorage();
-        } else {
-            return new CPTOrderStorage();
-        }
     }
 }
